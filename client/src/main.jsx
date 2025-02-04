b3bcedc70ccab4984da546f966738d6d0553c167
KaT0p <pattaradanaic43@gmail.com>
2025-02-04 16:17:42 +0700

    [เพิ่ม] Element Root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(  // สร้าง root และแสดงผลลัพธ์ใน element ที่มี id="root"
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root') // แสดงผลลัพธ์ใน element ที่มี id="root"
 )
